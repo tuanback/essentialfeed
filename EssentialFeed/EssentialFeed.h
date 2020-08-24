ec5b4586650586051ac64ba1d400f41ad0a4ccce
Do Thanh Tuan <tuando@next-aeon.com>
2020-08-24 21:11:35 +0900

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Real Life Swift on 2020/08/24.
+//  Copyright © 2020 Real Life Swift. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
