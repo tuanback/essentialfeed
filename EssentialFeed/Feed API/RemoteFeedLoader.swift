cecf3b6de0442ee0426a7a334cd62762ff5bcc93
Do Thanh Tuan <tuando@next-aeon.com>
2020-08-29 09:55:10 +0900

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -30,8 +30,8 @@ public final class RemoteFeedLoader {
     client.get(from: url) { result in
       switch result {
       case .success(let (data, response)):
-        if let json = try? JSONSerialization.jsonObject(with: data) {
-          completion(.success([]))
+        if let root = try? JSONDecoder().decode(Root.self, from: data) {
+          completion(.success(root.items))
         }
         else {
           completion(.failure(.invalidData))
@@ -41,4 +41,8 @@ public final class RemoteFeedLoader {
       }
     }
   }
+}
+
+private struct Root: Decodable {
+  let items: [FeedItem]
 }
