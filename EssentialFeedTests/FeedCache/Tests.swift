1d388e70ec31ff4ee2205830b36dde15e60e96bc
Do Thanh Tuan <tuando@next-aeon.com>
2020-09-07 21:02:55 +0900

    Add track memory leak to tests

--- EssentialFeedTests/FeedCache/Tests.swift
+++ EssentialFeedTests/FeedCache/Tests.swift
@@ -32,14 +32,12 @@ class FeedStore {
 class CacheFeedUseCaseTests: XCTestCase {
 
   func test_init_doesNotDeleteCachUponCreation() {
-    let store = FeedStore()
-    _ = LocalFeedLoader(store: store)
+    let (_, store) = makeSUT()
     XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
   }
 
   func test_save_requestsCacheDeleteion() {
-    let store = FeedStore()
-    let sut = LocalFeedLoader(store: store)
+    let (sut, store) = makeSUT()
     let items = [uniqueItem(), uniqueItem()]
     
     sut.save(items)
@@ -48,6 +46,14 @@ class CacheFeedUseCaseTests: XCTestCase {
   }
   
   // MARK: - Helpers
+  
+  private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
+    let store = FeedStore()
+    let sut = LocalFeedLoader(store: store)
+    
+    trackForMemoryLeaks(sut, file: file, line: line)
+    return (sut, store)
+  }
   
   private func uniqueItem() -> FeedItem {
     return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
