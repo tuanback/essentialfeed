b307b93a594880574feb0b80f9fde6aaf7d3edf5
Do Thanh Tuan <tuando@3i.ai>
2020-10-02 15:51:28 +0900

    Refactor code

---
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Tuan on 2020/10/02.
+//  Copyright © 2020 Real Life Swift. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+  typealias DeletionCompletion = (Error?) -> Void
+  typealias InsertionCompletion = (Error?) -> Void
+  
+  func deleteCachedFeed(completion: @escaping DeletionCompletion)
+  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -12,12 +12,15 @@ public class LocalFeedLoader {
   
   private var store: FeedStore
   private let currentDate: () -> Date
+  
+  public typealias SaveResult = Error?
+  
   public init(store: FeedStore, currentDate: @escaping () -> Date) {
     self.store = store
     self.currentDate = currentDate
   }
   
-  public func save(_ items: [FeedItem], completion: @escaping (Error?)->Void) {
+  public func save(_ items: [FeedItem], completion: @escaping (SaveResult)->Void) {
     
     store.deleteCachedFeed { [weak self] error in
       guard let self = self else { return }
@@ -29,18 +32,10 @@ public class LocalFeedLoader {
     }
   }
   
-  private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+  private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
     self.store.insert(items, timestamp: currentDate()) { [weak self] error in
       guard self != nil else { return }
       completion(error)
     }
   }
-}
-
-public protocol FeedStore {
-  typealias DeletionCompletion = (Error?) -> Void
-  typealias InsertionCompletion = (Error?) -> Void
-  
-  func deleteCachedFeed(completion: @escaping DeletionCompletion)
-  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
 }
