defd2674bab9e58f8f8a70782db3875ab83e35c7
Do Thanh Tuan <tuando@next-aeon.com>
2020-10-06 21:52:24 +0900

    Move `Codable` conformance from the framework-agnostic `LocalFeedImage` type to the new framework-specific `CodableFeeImage` type. The `CodableFeedImage` is a private type within the framework implementation since the `Codable` requirement is a framework-specific detail

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -11,8 +11,30 @@ import EssentialFeed
 
 class CodableFeedStore {
   private struct Cache: Codable {
-    let feed: [LocalFeedImage]
+    let feed: [CodableFeedImage]
     let timestamp: Date
+    
+    var localFeed: [LocalFeedImage] {
+      return feed.map { $0.local }
+    }
+  }
+  
+  private struct CodableFeedImage: Codable {
+    private let id: UUID
+    private let description: String?
+    private let location: String?
+    private let url: URL
+    
+    init(_ image: LocalFeedImage) {
+      id = image.id
+      description = image.description
+      location = image.location
+      url = image.url
+    }
+    
+    var local: LocalFeedImage {
+      return LocalFeedImage(id: id, description: description, location: location, url: url)
+    }
   }
   
   private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
@@ -24,12 +46,12 @@ class CodableFeedStore {
     
     let decoder = JSONDecoder()
     let cache = try! decoder.decode(Cache.self, from: data)
-    completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+    completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
   }
   
   func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
     let encoder = JSONEncoder()
-    let encoded = try! encoder.encode(Cache(feed: items, timestamp: timestamp))
+    let encoded = try! encoder.encode(Cache(feed: items.map(CodableFeedImage.init), timestamp: timestamp))
     try! encoded.write(to: storeURL)
     completion(nil)
   }
@@ -38,17 +60,15 @@ class CodableFeedStore {
 class CodableFeedStoreTests: XCTestCase {
   
   override class func setUp() {
-    super.setUp()
-    
     let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
     try? FileManager.default.removeItem(at: storeURL)
+    super.setUp()
   }
   
   override class func tearDown() {
-    super.tearDown()
-    
     let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
     try? FileManager.default.removeItem(at: storeURL)
+    super.tearDown()
   }
   
   func test_retrieve_deliversEmptyOnEmptyCache() {
